4117a768057b5485dbf22687e607637c1c296e65
Bhavesh Bhatt <bhattbhavesh91@gmail.com>
2018-09-21 23:11:24 +0530

    Matplotlib Session More Example added  v1.0.3.21.09.2018

--- matplotlib/matplotlib_questions.py
+++ matplotlib/matplotlib_questions.py
@@ -36,3 +36,36 @@ def plot4 ():
 	plt.ylabel('$y$ axis', size=24)
 	plt.legend(prop={'size': 24})
 	return plt.show()
+
+def plot5 ():
+	x = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+	popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+	x_pos = [i for i, _ in enumerate(x)]
+	plt.bar(x_pos, popularity, color=['red', 'black', 'green', 'blue', 'yellow', 'cyan'])
+	plt.xlabel("Languages")
+	plt.ylabel("Popularity")
+	plt.title("PopularitY of Programming Language\n" + "Worldwide, Oct 2017 compared to a year ago")
+	plt.xticks(x_pos, x)
+	return plt.show()
+
+def plot6():
+	n_groups = 5
+	men_means = (22, 30, 33, 30, 26)
+	women_means = (25, 32, 30, 35, 29)
+
+	# create plot
+	fig, ax = plt.subplots()
+	index = numpy.arange(n_groups)
+	bar_width = 0.35
+	opacity = 0.8
+
+	rects1 = plt.bar(index, men_means, bar_width, alpha=opacity, color='g', label='Men')
+	rects2 = plt.bar(index + bar_width, women_means, bar_width, alpha=opacity, color='r', label='Women')
+
+	plt.xlabel('Person')
+	plt.ylabel('Scores')
+	plt.title('Scores by person')
+	plt.xticks(index + bar_width, ('G1', 'G2', 'G3', 'G4', 'G5'))
+	plt.legend()
+	return plt.show()
+
